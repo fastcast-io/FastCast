e5e530acb8a30b24616d4ece32d71d6a95214940
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-11-13 01:15:10 -0600

    Location feature fully working

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -35,8 +35,6 @@ namespace FastCast.Pages
 
             Session = _context.Session.ToList(); // THIS IS BAD BY THE WAY. WE ARE RETRIEVING ALL THE SESSIONS LOL
 
-            bool shouldNotAccessSession = false;
-
             Debug.WriteLine($"Latitude: {latitude}, Longitude: {longitude}");
             try
             {
@@ -46,11 +44,32 @@ namespace FastCast.Pages
 
                 FastCastCoordinate sessionCoord = new FastCastCoordinate(latitude: selectedSession.Latitude, longitude: selectedSession.Longitude);
                 FastCastCoordinate userCoord = new FastCastCoordinate(latitude: Double.Parse(latitude), longitude: Double.Parse(longitude));
-                Double difference = sessionCoord.GetDistanceTo(userCoord);
+                Double difference = sessionCoord.GetDistanceTo(userCoord); // Distance is in meters
 
-                if (difference > 100000) // Need to update
+                // TEST: AROUND A CLASS/OFFICE IN THE CS DEPT:
+                // LAT: 33.587446
+                // LONG: -101.875457 <- session can be saved with it
+
+                // AROUND MY HOUSE: 
+                // LAT: 33.588424
+                // LON: -101.858219
+
+
+                // WITHIN RANGE
+                // LAT: 33.588426
+                // LONG: -101.858248
+                // LESS THAN 30 meters
+
+                // SLIGHTLY OUT OF RANGE
+                // LAT: 35.588451
+                // LONG:-101.858705 
+                // => AROUND 45.90 m
+                // TODO: IMPLEMENT A RADIUS LOL BECAUSE THAT THING IS HIGHLY INACCURATE
+                // TODO: IMPLEMENT INPUT VALIDATION FOR THE LATITUDE AND LONGITUDE
+                Debug.WriteLine($"********************************************\n\n\nDIFFERENCE IS {difference}\n\n\n********************************************\n\n\n");
+                if (difference > 40) // Might Need to update. // Need to check Pythagorean distance as well if the longitude and latitude are close,
+                                     // else, let's keep with spherical distance lol
                 {
-                    shouldNotAccessSession = true;
                     var ex = new Exception("");
                     ex.Data.Add("LOCATION ERROR", "Your longitude and latitude is not in the region specified by the initiator. Get closer");
                     throw ex;
