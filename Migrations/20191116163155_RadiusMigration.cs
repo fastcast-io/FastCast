e08f7af26868c417106a5e3bcf630d6f575c1007
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-11-16 10:34:02 -0600

    Added the Radius field and its migration

---
+++ Migrations/20191116163155_RadiusMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FastCast.Migrations
+{
+    public partial class RadiusMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Radius",
+                table: "Session",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Radius",
+                table: "Session");
+        }
+    }
+}
