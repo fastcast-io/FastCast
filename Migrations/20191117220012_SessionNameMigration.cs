af3522a31691f57ca5f478e20ab411248628729a
N'Godjigui Junior Diarrassouba <jndiarrassouba@gmail.com>
2019-11-17 16:34:25 -0600

    Added session name and migrations

---
+++ Migrations/20191117220012_SessionNameMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FastCast.Migrations
+{
+    public partial class SessionNameMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SessionName",
+                table: "Session",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SessionName",
+                table: "Session");
+        }
+    }
+}
